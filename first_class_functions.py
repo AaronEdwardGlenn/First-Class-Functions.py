a00b927d9f10d7c9cc910abddb840aea3c7c6e4c
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-24 23:22:51 -0700

    created multiple examples of passing functions as arguments

--- first_class_functions.py
+++ first_class_functions.py
@@ -17,4 +17,25 @@ print(f(5))
 
 # so we have assigned a function as a variable
 
-# we can also pass functions as arguments
+# we can also pass functions as arguments and return functions as a result of other functions --> these are higher order functions.
+
+
+def cube(x):
+    return x * x * x
+
+
+def my_map(func, arg_list):
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
+
+
+squares = my_map(cube, [1, 2, 3, 4, 5])
+
+print(squares)
+
+
+# so a lot happening above. we are creating the my_map function, which takes a function. we are creating the result empty array, looping through the arg_list, appending to our empy array each item from the arg_list after it has passed through the function that is an argument.
+
+# i added the cube function to this, and now the results are cubed. so it's helpful to see here why a higher order function would be useful.
