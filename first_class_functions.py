43e67d2216a6ac9f45031e5d4a1f14d0c01f5a1b
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-24 23:02:09 -0700

    set file arch

---
+++ first_class_functions.py
@@ -0,0 +1,1 @@
+# a first class function is an entity which supports all the operations generally available to other entities. These operations typically include being passed as an argumetn, returned from a function, and assigned to a variable.
